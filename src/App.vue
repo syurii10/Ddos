e0530a74246b1dedccfe585c3dfc5160c1c29065
Bratenko Alexander <bratenko6@gmail.com>
2023-11-03 13:11:05 +0200

    (fix) Compleate template

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <router-view v-slot="{ Component }">
+    <keep-alive exclude="ProfilePage">
+      <component :is="Component" />
+    </keep-alive>
+  </router-view>
 </template>
 
 <style scoped>
